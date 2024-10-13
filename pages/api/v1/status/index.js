1434d006e538524679498443b00d259e511816c5
Luiz-Mattos14 <luiz.esmattos@gmail.com>
2024-10-13 14:17:24 -0300

    Feat: add jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
+  console.log("A função status foi chamada");
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  console.log("Resultado da consulta:", result.rows);
 
   response.status(200).json({
     chave: "Api está funcionando",
