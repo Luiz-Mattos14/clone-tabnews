cdab80508ab8bae1274644e69d44a65dea0faf8a
Luiz-Mattos14 <luiz.esmattos@gmail.com>
2024-11-01 00:10:23 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  //Espera que no responseBody.update_at seja igual à parsedUpdateAt
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  //Espera que no dependencies.database.version seja igual à 16.0 - STRING
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  //Espera que no dependencies.database.max_connections seja igual à 100 - NUMBER
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
